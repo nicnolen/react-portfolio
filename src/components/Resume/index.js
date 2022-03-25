59276ae85c7581e6a90de1a51a0c7d7a03a51de8
Nicholas Nolen <nicnolen@ymail.com>
2022-03-25 13:59:11 -0500

    changed skills from About component to Resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import resume from '../../assets/images/portfolioImages/resume.pdf';
+import FrontendSkills from '../FrontendSkills';
+import BackendSkills from '../BackendSkills';
+import TechnicalSkills from '../TechnicalSkills';
+import skillsInfo from './skills.json';
 
 function Resume() {
+  const Wrapper = props => {
+    return <div className='wrapper'>{props.children}</div>;
+  };
   return (
     <section className='container'>
       <h2 className='top-title'>Resume</h2>
-
-      <a href={resume} target='_blank' rel='noreferrer'>
-        Download Resume
-      </a>
+      <div>
+        <a href={resume} target='_blank' rel='noreferrer'>
+          Download Resume
+        </a>
+      </div>
+      <div>
+        <h3>Skills</h3>
+        <Wrapper id='card-data'>
+          <h4>Front End Skills</h4>
+          {skillsInfo.map((skill, i) => (
+            <FrontendSkills
+              key={i}
+              name={skill.name}
+              category={skill.category}
+              documentation={skill.documentation}
+            />
+          ))}
+          <h4>Back End Skills</h4>
+          {skillsInfo.map((skill, i) => (
+            <BackendSkills
+              key={i}
+              name={skill.name}
+              category={skill.category}
+              documentation={skill.documentation}
+            />
+          ))}
+          <h4>Technical Skills</h4>
+          {skillsInfo.map((skill, i) => (
+            <TechnicalSkills
+              key={i}
+              name={skill.name}
+              category={skill.category}
+              documentation={skill.documentation}
+            />
+          ))}
+        </Wrapper>
+      </div>
     </section>
   );
 }
