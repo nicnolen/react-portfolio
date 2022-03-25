35143168ed4912ab00225ee13d4f355073d540f5
Nicholas Nolen <nicnolen@ymail.com>
2022-03-25 14:19:31 -0500

    set up a handle change function to handle errors

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,10 +7,29 @@ function ContactForm() {
     email: '',
     message: '',
   });
-
   const [errorMessage, setErrorMessage] = useState('');
   const { name, email, message } = formState;
   
+  // Handle change function 
+  const handleChange = e => {
+    // validate email
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      // isValid conditional statement 
+      if (!isValid) {
+        setErrorMessage('Invalid email format!');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      // error message for blank elements
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required!`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+  }
   return (  );
 }
 
