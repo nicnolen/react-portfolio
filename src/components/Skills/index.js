1f3680e20a732191b923834a87606e1082eaec77
Nicholas Nolen <nicnolen@ymail.com>
2022-03-25 09:45:23 -0500

    conditionally rendered skills to a list in the Skills component

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -2,22 +2,38 @@ import React from 'react';
 
 function Skills({ name, category, documentation }) {
   return (
-    <span>
-      <h3>Front End Skills</h3>
-      <ul>
-        <li>
-          <a href={documentation}>{name}</a>
-        </li>
-      </ul>
-      <h3>Back End Skills</h3>
-      <ul>
-      
-      </ul>
-      <h3>Technical Skills</h3>
-      <ul>
-       
-      </ul>
-    </span>
+    <div>
+      {category === 'frontend' && (
+        <React.Fragment>
+          <h3>Front End Skills</h3>
+          <ul>
+            <li>
+              <a href={documentation}>{name}</a>
+            </li>
+          </ul>
+        </React.Fragment>
+      )}
+      {category === 'backend' && (
+        <React.Fragment>
+          <h3>Back End Skills</h3>
+          <ul>
+            <li>
+              <a href={documentation}>{name}</a>
+            </li>
+          </ul>
+        </React.Fragment>
+      )}
+      {category === 'technical' && (
+        <React.Fragment>
+          <h3>Technical Skills</h3>
+          <ul>
+            <li>
+              <a href={documentation}>{name}</a>
+            </li>
+          </ul>
+        </React.Fragment>
+      )}
+    </div>
   );
 }
 
