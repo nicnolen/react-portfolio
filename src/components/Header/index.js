7ffcc423dc4547293146cb81260647017e643afa
Nicholas Nolen <nicnolen@ymail.com>
2022-03-28 09:23:55 -0500

    set up useState and useEffect to map over Navigation component links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import Project from '../Portfolio';
 import About from '../About';
@@ -7,11 +7,38 @@ import Resume from '../Resume';
 import Contact from '../Contact';
 
 function Header() {
+  // conditionally render the Contact component
+  const [aboutSelected, setAboutSelected] = useState(false);
+  // set categories
+  const [categories] = useState([
+    {
+      name: 'project',
+      description:
+        'Listed out my top six projects with links to the deployed link and the GitHub link',
+    },
+    {
+      name: 'contact',
+      description: 'Form to contact me',
+    },
+    {
+      name: 'resume',
+      description:
+        'Link to download my resume as well as a list of my skills with links to the documentation for the front end and back end skills',
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <header>
       <HashRouter>
         <nav>
-          <Navigation />
+          <Navigation
+            categories={categories}
+            setCurrentCategory={setCurrentCategory}
+            currentCategory={currentCategory}
+            aboutSelected={aboutSelected}
+            setAboutSelected={setAboutSelected}
+          />
         </nav>
 
         <div>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 import { Navbar, Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import '../../assets/css/Navigation.css'
+import '../../assets/css/Navigation.css';
 
-function Navigation() {
+function Navigation(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+    aboutSelected,
+    setAboutSelected,
+  } = props;
+
+  const categorySelected = name => {
+    console.info(`${name} selected`);
+    return name;
+  };
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  });
   return (
     <div>
       <Navbar expand='lg' bg='dark' sticky='top'>
@@ -15,26 +32,37 @@ function Navigation() {
 
         <Container>
           <ul class='navbar-nav ml-auto navitem-indent'>
-            <li class='nav-item'>
-              <NavLink to='/about'>
+            <li class={`mx-2 nav-item ${aboutSelected && 'navActive'}`}>
+              <NavLink
+                to='/about'
+                onClick={() => {
+                  categorySelected('About Me');
+                  setAboutSelected(true);
+                }}>
                 <span class='text-light nav-font'>About Me</span>
               </NavLink>
             </li>
-            <li class='nav-item'>
-              <NavLink to='/project'>
-                <span class='text-light nav-font'>Projects</span>
-              </NavLink>
-            </li>
-            <li class='nav-item'>
-              <NavLink to='/contact'>
-                <span class='text-light nav-font'>Contact</span>
-              </NavLink>
-            </li>
-            <li class='nav-item'>
-              <NavLink to='/resume'>
-                <span class='text-light nav-font'>Resume</span>
-              </NavLink>
-            </li>
+            {categories.map(category => (
+              // whenever you map over anything in JSX the outmost element must have a key attribute set to something unique
+              <li
+                className={`mx-1 'nav-item' ${
+                  currentCategory.name === category.name &&
+                  !aboutSelected &&
+                  'navActive'
+                }`}
+                key={category.name}>
+                <NavLink to={`/${category.name}`}>
+                  <span
+                    onClick={() => {
+                      setCurrentCategory(category);
+                      categorySelected(category.name);
+                      setAboutSelected(false);
+                    }}>
+                    {capitalizeFirstLetter(category.name)}
+                  </span>
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </Container>
       </Navbar>
