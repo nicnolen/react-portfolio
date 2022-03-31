660600fa21bd3ae5578a98671eafbd36c6de5ae7
Nicholas Nolen <nicnolen@ymail.com>
2022-03-31 17:57:47 -0500

    added a santize to SVG images

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ function Footer() {
       <a href="https://github.com/nicnolen?tab=repositories">
         <h4>GitHub</h4>
         <img
-          src="../../assets/images/icons/github.svg"
+          src="../../assets/images/icons/github.svg?sanitize=true"
           alt="GitHub"
         
           target="_blank"
@@ -16,7 +16,7 @@ function Footer() {
       <a href="https://www.linkedin.com/in/nicholasnolen/">
         <h4>LinkedIn</h4>
         <img
-          src="../../assets/images/icons/linkedin.svg"
+          src="../../assets/images/icons/linkedin.svg?sanitize=true"
           alt="LinkedIn"
           target="_blank"
         />
@@ -24,7 +24,7 @@ function Footer() {
       <a href="https://stackoverflow.com/users/16633801/nicholas-nolen">
         <h4>Stack Overflow</h4>
         <img
-          src="../../assets/images/icons/stack-overflow.svg"
+          src="../../assets/images/icons/stack-overflow.svg?sanitize=true"
           alt="Stack Overflow"
           target="_blank"
         />
