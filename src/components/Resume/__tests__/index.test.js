088d81d766cfd694a40d88ca9d7a9aad42d6c5bd
Nicholas Nolen <nicnolen@ymail.com>
2022-04-22 11:09:48 -0500

    updated Resume test

--- src/components/Resume/__tests__/index.test.js
+++ src/components/Resume/__tests__/index.test.js
@@ -2,21 +2,21 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Portfolio from '..';
+import Resume from '..';
 
 // After each test, make sure there isnt any leftover memory that could give you false results
 afterEach(cleanup);
 
 // Declare the components you are testing
-describe('Portfolio component', () => {
+describe('Resume component', () => {
   // verify the component is rendering properly
-  it('Portfolio component renders', () => {
-    render(<Portfolio />);
+  it('Resume component renders', () => {
+    render(<Resume />);
   });
 
   // compare snapshot versions of the DOM node structure
   it('matches snapshot DOM node structure', () => {
-    const { asFragment } = render(<Portfolio />);
+    const { asFragment } = render(<Resume />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
